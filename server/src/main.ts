6ea25a786d6bc2a11dac10332ca9e17966098fef
Jerry-Khobby <jerrymardeburg@gmail.com>
2025-10-18 00:49:26 -1200

    websocket, done, now testing

--- server/src/main.ts
+++ server/src/main.ts
@@ -6,6 +6,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.useWebSocketAdapter(new IoAdapter(app));
+
+    // Enable CORS for WebSocket
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
